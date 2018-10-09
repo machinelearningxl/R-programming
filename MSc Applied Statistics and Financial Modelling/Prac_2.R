d0b7c224deb92c0e2205a799203d099fe5539730
Antero Maripuu <antero.maripuu@gmail.com>
2018-10-09 12:56:26 +0100

    Antero Maripuu

--- MSc Applied Statistics and Financial Modelling/Prac_2.R
+++ MSc Applied Statistics and Financial Modelling/Prac_2.R
@@ -2,4 +2,4 @@
 # Date: 08/10/2018
 # Homework in MSc Applied Statistics and Financial Modelling (University of London)
 # Course: Statistical Analysis (2018/2019)
-# All rights reserved.
+# All rights reserved
