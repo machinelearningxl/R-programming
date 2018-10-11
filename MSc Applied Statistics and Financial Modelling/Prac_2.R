a26b44886c200cd1574847bd2cc96312cc4ea0b6
Antero Maripuu <antero.maripuu@gmail.com>
2018-10-11 16:27:17 +0100

    UPdate

--- MSc Applied Statistics and Financial Modelling/Prac_2.R
+++ MSc Applied Statistics and Financial Modelling/Prac_2.R
@@ -7,6 +7,17 @@
 #Data Handeline in R
 getwd() #working directory
 
+#Random example
+
+groceries <- c(227.01, 241.42, 188.08, 238.23, 235.86)
+leisure <- c(96.98, 140.44, 85.13, 158.22, 103.06)
+income <- c(741.29, 854.07, 812.07, 813.69, 731.42)
+spend <- data.frame(groceries, leisure, income)
+
+
+
+############################################################################################
+
 x<-read.table("oil.txt", header=FALSE)
 names(x)<-c("spirit","gravity","pressure","distil","endpoint")
 x
@@ -25,3 +36,4 @@ oil<-lm(spirit~endpoint,data=x)
 
 
 #Which of the four explanatory variables appears to have the strongest linear relationship indiviually with spirit?
+
